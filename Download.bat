a89660042b1a4c298163190887f45226687ed7e9
G67890 <135536042+G67890@users.noreply.github.com>
2023-07-08 23:19:18 +0530

    Update Download.bat

--- Download.bat
+++ Download.bat
@@ -1,4 +1,3 @@
 @echo off
-choco upgrade chocolatey -y
-choco install anydesk -y
+powershell -Command "choco install anydesk -y"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/G67890/Nw/main/Start.bat -OutFile start.bat
